5b477c6e387c87d531f4d6cde559153c8ba8ac2a
glebsarabajko-boop <glebsarabajko@gmail.com>
2025-11-04 19:10:35 +0300

    777

---
+++ монте-карло/монте-карло.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <random>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	srand(time(0));
+	int n, k=0;
+	float  x_min= -3.0, x_max = 3.0, y_min = -3.0, y_max = 3.0, x, y, pi=3.1416;
+	cout << "Введите количество точек" << endl;
+	cin >> n;
+	float S = (x_max-x_min)*(y_max-y_min);
+	float r = (x_max - x_min) / 2;
+	for (int i = 0; i < n; i++) {
+		x = x_min + (x_max-x_min) * rand() /(RAND_MAX+1);
+		y = y_min + (y_max - y_min) * rand() / (RAND_MAX + 1);
+
+		if (pow(x , 2) + pow(y , 2) <= pow(r, 2))
+			k+=1;
+	}
+	float s1 = S * k / n;
+	cout << "Точек в круге: " << k << " из " << n << endl;
+	cout << "Площадь круга методом Монте-Карло: "<<fixed<< setprecision(4) << s1  << endl;
+	cout << "Площадь круга по формуле: " << fixed << setprecision(4) << pow(r, 2) * pi << endl;
+	return 0;
+}
---
+++ площадь_треугольника/площадь_треугольника.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <random>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	srand(time(0));
+	float y_min = 0.0, y_max = 3.0, x_min = 0.0, x_max = 4.0, x, y;
+	int n, k=0;
+	cout << "Введите количество точек: " << endl;
+	cin >> n;
+	float s1 = (y_max - y_min) * (x_max - x_min), tg=-y_max/x_max, s2;
+	for (int i=0; i < n; i++) {
+		y = y_min + (y_max - y_min) * rand() / (RAND_MAX + 1);
+		x = x_min + (x_max - x_min) * rand() / (RAND_MAX + 1);
+		if (y <= tg * x + y_max) {
+			k++;
+		}
+	}
+	s2 = s1 * k / n;
+	cout << "Точек в треугольнике: " << k << " из " << n << endl;
+	cout << "Площадь треугольника методом Монте-Карло: " << fixed << setprecision(4) << s2 << endl;
+	cout << "Площадь треугольника по формуле: " << fixed << setprecision(4) << s1/2 << endl;
+	return 0;
+}
